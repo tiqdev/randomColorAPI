a75fe12b12dacdd98ef699143fe9ca8deea459ce
Tarık KAYA <tiqqq24@gmail.com>
2020-05-27 16:16:45 +0300

    ffirst commit

---
+++ color.py
@@ -0,0 +1,36 @@
+import random
+import json
+
+
+class GiveMeAColor:
+
+    # create random color rgb code
+    def randomRGB(self):
+        rColor = random.randint(0, 255)
+        gColor = random.randint(0, 255)
+        bColor = random.randint(0, 255)
+        response = f'rgb{rColor,gColor,bColor}'
+        return response
+
+    # create random color HEX code
+    def randomHex(self):
+        rColor = random.randint(0, 255)
+        gColor = random.randint(0, 255)
+        bColor = random.randint(0, 255)
+        hexCode = '{:02x}{:02x}{:02x}'.format(
+            rColor, gColor, bColor).upper()
+        return hexCode
+
+    # create random color with rgb,rgba and hex code
+    def generateColor(self):
+        rColor = random.randint(0, 255)
+        gColor = random.randint(0, 255)
+        bColor = random.randint(0, 255)
+        hexCode = '{:02x}{:02x}{:02x}'.format(rColor, gColor, bColor).upper()
+        color = {
+            "HEX": hexCode,
+            "RGB": f'rgb{rColor,gColor,bColor}',
+            "RGBA": f'rgb{rColor,gColor,bColor,1}'
+        }
+        responseColor = json.dumps(color)
+        return responseColor
---
+++ randomColor.py
@@ -0,0 +1,32 @@
+from color import GiveMeAColor
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+randomColor = GiveMeAColor()
+
+
+@app.route("/")
+def hello():
+    return jsonify({"data": "tiqdesign random_color_api"})
+
+
+@app.route("/random", methods=['GET'])
+def generateColor():
+    result = randomColor.generateColor()
+    return result, 200
+
+
+@app.route("/randomRGB", methods=['GET'])
+def generateRGB():
+    result = randomColor.randomRGB()
+    return jsonify({"RGB": result}), 200
+
+
+@app.route("/randomHEX", methods=['GET'])
+def generateHEX():
+    result = randomColor.randomHex()
+    return jsonify({"HEX": result}), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
